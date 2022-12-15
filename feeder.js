71234199255df76c053b771ed2f2c08a3da14ce4
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-12-15 14:45:27 +0700

    finalized.

--- feeder.js
+++
@@ -1,7 +0,0 @@
-const users = ['staff:staff', 'sales:sales'];
-
-users.forEach(user => {
-  const token = Buffer.from(user).toString('base64');
-
-  console.log(user, token);
-});
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,8 +4,6 @@ module.exports = (req, res, next) => {
   let token = req.get('authorization');
   token = token ? token.replace('Bearer ', '') : token;
 
-  console.log('auth', token, users[token]);
-
   if (!req.url.includes('/login') && !token) {
     res.status(401).json({ message: 'Missing the required authorization token.' });
   } else if (!req.url.includes('/login') && token && !users[token]) {
